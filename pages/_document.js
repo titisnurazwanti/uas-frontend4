d2b0aa45ba7853ee6d1b429fda87980a66eb4434
titisnurazwanti <titisnurazwanti03@gmail.com>
2022-07-08 19:08:02 +0700

    menambahkan folder

---
+++ pages/_document.js
@@ -0,0 +1,67 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <div>
+  <meta charSet="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content />
+  <meta name="author" content />
+  <title>Portofolio Titis Nur Azwanti</title>
+  <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+  
+  {/* Font Awesome icons (free version)*/}
+  <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+  {/* Google fonts*/}
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+  {/* Core theme CSS (includes Bootstrap)*/}
+  <link href="assets/css/style.css" rel="stylesheet" />
+</div>
+
+      {/* <Head>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <title>Portofolio</title>
+        <meta name="description" content />
+        <meta name="author" content />
+        <meta name="keywords" content />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
+          rel="stylesheet"
+        />
+        <link
+          rel="meme-icon"
+          sizes="180x180"
+          href="assets/img/logo.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon.ico"
+        />
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="assets/css/style.css" rel="stylesheet" />
+        <link href="assets/css/responsive.css" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="assets/js/skrollr.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-progressbar/0.9.0/bootstrap-progressbar.min.js"></script>
+        <script src="assets/js/jquery.countTo.min.js"></script>
+        <script src="assets/js/script.js"></script>
+      </body> */}
+    </Html>
+  );
+}
